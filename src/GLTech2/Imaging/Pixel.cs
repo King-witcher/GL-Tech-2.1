7d1e9416557b49e0d37773f2feb1c6e884676883
King-witcher <giuseppe.2000@hotmail.com>
2021-11-15 22:26:41 -0300

    RGB fields to private RGB.ToString

--- src/GLTech2/Imaging/Pixel.cs
+++ src/GLTech2/Imaging/Pixel.cs
@@ -7,68 +7,57 @@ namespace GLTech2.Imaging
     [StructLayout(LayoutKind.Explicit)]
     public struct Pixel
     {
-        //Union
-        [FieldOffset(0)]
-        internal uint rgb;
+        // Union
+        [FieldOffset(0)] uint uint_value;
 
-        [FieldOffset(0)]
-        internal byte b;
+        [FieldOffset(3)] byte a;
+        [FieldOffset(2)] byte r;
+        [FieldOffset(1)] byte g;
+        [FieldOffset(0)] byte b;
 
-        [FieldOffset(1)]
-        internal byte g;
-
-        [FieldOffset(2)]
-        internal byte r;
-
-        [FieldOffset(3)]
-        private byte a;
+        public byte R => r;
+        public byte G => g;
+        public byte B => b;
 
         public Pixel(byte red, byte green, byte blue)
         {
-            rgb = 0;
-            a = 255;
+            uint_value = default;
+            a = 0xff;
             r = red;
             g = green;
             b = blue;
         }
 
-        public float Luma => (0.2126f * r * r + 0.7152f * g * g + 0.0722f * b * b) / (255f * 255f);
+        public float Luma => (0.2126f * R * R + 0.7152f * G * G + 0.0722f * B * B) / (255f * 255f);
 
-        public float FastLuma => (0.2126f * r + 0.7152f * g + 0.0722f * b) / (255f);
+        public float FastLuma => (0.2126f * R + 0.7152f * G + 0.0722f * B) / (255f);
 
-        public byte Brightness => (byte)((r + g + b) / 3);
+        public byte Brightness => (byte)((R + G + B) / 3);
 
-        public byte R => r;
-
-        public byte G => g;
-
-        public byte B => b;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [Obsolete]
         public Pixel Average(Pixel rgb)
         {
-            rgb.r = (byte)((r + rgb.r) >> 1);
-            rgb.g = (byte)((g + rgb.g) >> 1);
-            rgb.b = (byte)((b + rgb.b) >> 1);
+            rgb.r = (byte)((R + rgb.R) >> 1);
+            rgb.g = (byte)((G + rgb.G) >> 1);
+            rgb.b = (byte)((B + rgb.B) >> 1);
 
             return rgb;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Pixel Mix(Pixel rgb, float factor)
         {
             ushort parcel1, parcel2;
 
-            parcel1 = (ushort)(r * (1 - factor));
-            parcel2 = (ushort)(rgb.r * factor);
+            parcel1 = (ushort)(R * (1 - factor));
+            parcel2 = (ushort)(rgb.R * factor);
             rgb.r = (byte)(parcel1 + parcel2);
 
-            parcel1 = (ushort)(g * (1 - factor));
-            parcel2 = (ushort)(rgb.g * factor);
+            parcel1 = (ushort)(G * (1 - factor));
+            parcel2 = (ushort)(rgb.G * factor);
             rgb.g = (byte)(parcel1 + parcel2);
 
-            parcel1 = (ushort)(b * (1 - factor));
-            parcel2 = (ushort)(rgb.b * factor);
+            parcel1 = (ushort)(B * (1 - factor));
+            parcel2 = (ushort)(rgb.B * factor);
             rgb.b = (byte)(parcel1 + parcel2);
 
             return rgb;
@@ -78,13 +67,13 @@ namespace GLTech2.Imaging
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Pixel operator *(Pixel rgb, float factor)
         {
-            ulong red = (ulong)(rgb.r * factor);
+            ulong red = (ulong)(rgb.R * factor);
             if (red > 255)
                 red = 255;
-            ulong green = (ulong)(rgb.g * factor);
+            ulong green = (ulong)(rgb.G * factor);
             if (green > 255)
                 green = 255;
-            ulong blue = (ulong)(rgb.b * factor);
+            ulong blue = (ulong)(rgb.B * factor);
             if (blue > 255)
                 blue = 255;
 
@@ -95,11 +84,16 @@ namespace GLTech2.Imaging
             return rgb;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator uint(Pixel rgb) => rgb.rgb;
+        public override string ToString()
+        {
+            return $"RGB<{r}, {g}, {b}>";
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator Pixel(uint rgb) => new Pixel { rgb = rgb };
+        public static implicit operator uint(Pixel rgb) => rgb.uint_value;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator Pixel(uint rgb) => new Pixel { uint_value = rgb };
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator Pixel((byte r, byte g, byte b) components)
--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -8,6 +8,10 @@ namespace Test
         {
             Debug.OpenConsole();
 
+            GLTech2.Imaging.Pixel a = 0x_ff_80_40_20;
+            System.Console.WriteLine(a.ToString());
+            System.Console.ReadKey();
+
             E1M1();
 
             Debug.Log("Releasing resources...");

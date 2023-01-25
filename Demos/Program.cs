62969366c0a671ceaffe1d24c45f81cf6e8a3658
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2023-01-24 23:38:57 -0300

    msg

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -1,63 +1,34 @@
-﻿using Engine;
-using Engine.World;
-using Engine.World.Composed;
-using Engine.Imaging;
-using Engine.Scripting;
-using Engine.Scripting.Prefab;
-using System.Runtime.InteropServices;
-using System.Diagnostics;
-
-using System;
+﻿using Engine.World;
 
 namespace Engine.Demos
 {
     unsafe static internal partial class Program
     {
+        static Renderer renderer = new Renderer();
+
         static void Main()
         {
-            RenderDemos();
+            SetupRenderer();
+
+            Play<Wolfenstein.Map>();
+            Play<SuperBlockMap.Map>();
         }
 
-        static void RenderDemos()
+        static void SetupRenderer()
         {
             Debug.OpenConsole();
-
-            // Renderer customization
-            // Renderer.FullScreen = true;
-
-            Renderer renderer = new Renderer();
-
+            renderer = new Renderer();
             renderer.CustomHeight = 600;
             renderer.CustomWidth = 800;
-            renderer.FieldOfView = 110f;
+            renderer.FieldOfView = 72f;
             renderer.SynchronizeThreads = true;
             renderer.CaptureMouse = true;
+        }
 
-            // Mapa pequeno para exemplificar o funcionamento do BlockMap
-            FloorStressTest.Map floorStressTest = new();
-            renderer.Play(floorStressTest);
-            floorStressTest.Dispose();
-
-            // Mapa pequeno para exemplificar o funcionamento do BlockMap
-            SampleBlockMap.Map sampleBlockMap = new();
-            renderer.Play(sampleBlockMap);
-            sampleBlockMap.Dispose();
-
-            // Mapa que demonstra o funcionamento do sistema de parenting e do background
-            RotatingPillars.Map pillarsMap = new();
-            renderer.CaptureMouse = true;
-            renderer.Play(pillarsMap);
-            pillarsMap.Dispose();
-
-            // Primeiro mapa do jogo Wolfenstein 3D - clássico da computação gráfica por usar a técnica de RayCasting
-            Wolfenstein.Map wolfenstein = new();
-            renderer.Play(wolfenstein);
-            wolfenstein.Dispose();
-
-            // Teste de estresse com um blockmap composto por muitas paredes com tratamento de colisão
-            SuperBlockMap.Map superBlockMap = new();
-            renderer.Play(superBlockMap);
-            superBlockMap.Dispose();
+        static void Play<Map>() where Map : Scene, new()
+        {
+            using Map map = new();
+            renderer.Play(map);
         }
     }
 }
